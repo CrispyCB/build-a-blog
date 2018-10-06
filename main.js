011e58338524b475553f0746818bff1e7d52f811
CrispyCB <cebahnweg@gmail.com>
2018-10-06 17:19:18 -0400

    adds Python, Django and Spring

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 const front-ends = ["HTML/CSS?JS","React", "Angular","Vue", "Bootstrap", "Materalize","Foundation"];
-const back-ends = ["C#", "Java", "Node/Express","Go","Erlang","Clojure","Lisp","Racket","Ruby/Rails"];
+const back-ends = ["C#", "Java/Spring", "Node/Express","Go","Erlang","Clojure","Lisp","Racket","Ruby/Rails","Python/Django"];
 const databases = ["MongoDB","SQL","NoSQL","PostGreSQL","CouchDB","Redis","SQLite"];
