d05a2f5efaccd1a94e93a574db28f396461543df
CrispyCB <cebahnweg@gmail.com>
2018-10-06 17:22:59 -0400

    adds variables for later button assignment.

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+
 const front-ends = ["HTML/CSS?JS","React", "Angular","Vue", "Bootstrap", "Materalize","Foundation"];
 const back-ends = ["C#", "Java/Spring", "Node/Express","Go","Erlang","Clojure","Lisp","Racket","Ruby/Rails","Python/Django"];
 const databases = ["MongoDB","SQL","NoSQL","PostGreSQL","CouchDB","Redis","SQLite"];
+
+const new-front-end = document.getElementById('new-front-end');
+const new-back-end = document.getElementById('new-back-end');
+const new-db = document.getElementById('new-db');
+const whole-new-blog = document.getElementById('whole-new-blog');
