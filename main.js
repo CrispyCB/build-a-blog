fa5aeda797639ec0367a972122e90af8e318648c
CrispyCB <cebahnweg@gmail.com>
2018-10-06 17:04:36 -0400

    adds front-end, back-end, and database arrays

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-const front-ends = [];
-const back-ends = ["C#", "Java", "Node/Express","Go",];
-const databases = [];
+const front-ends = ["HTML/CSS?JS","React", "Angular","Vue", "Bootstrap", "Materalize","Foundation"];
+const back-ends = ["C#", "Java", "Node/Express","Go","Erlang","Clojure","Lisp","Racket","Ruby/Rails"];
+const databases = ["MongoDB","SQL","NoSQL","PostGreSQL","CouchDB","Redis","SQLite"];
