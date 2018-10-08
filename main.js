d0209dbd4d1c1bf226562d1e4081d9adf09437b8
CrispyCB <cebahnweg@gmail.com>
2018-10-07 20:08:29 -0400

    adds comments

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-
+//arrays of from-ends, back-ends and dbs
 const front-ends = ["HTML/CSS?JS","React", "Angular","Vue", "Bootstrap", "Materalize","Foundation"];
 const back-ends = ["C#", "Java/Spring", "Node/Express","Go","Erlang","Clojure","Lisp","Racket","Ruby/Rails","Python/Django"];
 const databases = ["MongoDB","SQL","NoSQL","PostGreSQL","CouchDB","Redis","SQLite"];
-
+//const variables for later button creation
 const new-front-end = document.getElementById('new-front-end');
 const new-back-end = document.getElementById('new-back-end');
 const new-db = document.getElementById('new-db');
